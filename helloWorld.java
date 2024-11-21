bede59fa158527476daf6bd78100cb481a35bf38
Khaun02 <khaunx02@gmail.com>
2024-11-21 14:10:36 -0600

    Update helloWorld.java
    tested out adding the node stuff to my helloWorld.java file
    then added some sysout lines as well
    works

--- helloWorld.java
+++ helloWorld.java
@@ -6,5 +6,12 @@ public class helloWorld {
         System.out.println("Shaun is the best");
 
         System.out.println("this was on the big pc gang");
+
+        Node<Integer> node1 = new Node<Integer>(10);
+        Node<Integer> node2 = new Node<Integer>(22);
+        node1.setNext(node2);
+
+        System.out.println("Node 1 Data: " + node1.getData());
+        System.out.println("Node 2 Data: " + node1.getNext().getData());
     }
 }
